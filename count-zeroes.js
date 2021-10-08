8e900c107df6e1f2c61b0d6cc1b579d649ef1a39
Jerry Hsu <Jerry>
2021-10-08 15:07:30 -0400

    count-zeros

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,18 @@
-function countZeroes() {
+function countZeroes(arr) {
+  let idx = Math.floor(arr.length / 2);
+
+  while (arr[idx] === arr[idx+1] && idx !== arr.length-1 && idx !== 0){
+    idx = ( arr[idx]===0 ? Math.floor(idx/2) : Math.floor(idx/2 + idx));
+  }
   
+  switch(idx) {
+    case 0:
+      return arr[idx] === 0 ? arr.length : arr.length - 1;
+    case arr.length -1:
+      return 0
+    default:
+      return arr.length - (idx + 1);
+  }
 }
 
 module.exports = countZeroes
